eeeb03efe1392fab8769e109c7c14c7c58d1e4b9
Karanfil, Mustafa Suphi Deniz <karanmus@fel.cvut.cz>
2023-01-21 14:49:28 +0100

    udp start

--- dkm.c
+++ dkm.c
@@ -1,23 +1,8 @@
 /* includes */
 
 #include "vxWorks.h"
-#include "motor.h"
-
 
 
 void start(void) {
-	int end_tasks = 0;
-	
-	taskSpawn("tMotor", 100, 0, 4096, (FUNCPTR) motorTask, &end_tasks, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	
-	char c;
-	while(1) {
-		c = (char)getchar();
-		if (c == 'q' || c == 'Q') break;
-	}
-	
-	end_tasks = 1;
-		
+
 }
-
-
