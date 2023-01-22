a22e5a3301e79a67b7879d34aa25421407df9efd
Jan Švrčina <svrc.jan@gmail.com>
2023-01-22 13:45:18 +0100

    dkm.c merge

--- dkm.c
+++ dkm.c
@@ -1,20 +1,26 @@
 /* includes */
 
 #include "vxWorks.h"
-#include "udp.h"
-#include <taskLib.h>
-/* I think we are only sending one int at each time:*/
-#define payload 4
-static int end;
-static UDP *udp;
 
-void start(char *ip_address, int port) {
+
+void start(void) {
+	int end_tasks = 0;
+	int target_steps = 0;
+	printf("Motor\n");
 	
-	udp = initudp(ip_address, port);
-
-	/* MASTER */
-	  sprintf(taskName, "tControlMotor");
-	  TASK_ID tControlMotor = taskSpawn(
-	  taskName, 110, 0, 4096, (FUNCPTR)getDesiredPos,
-	         (UDP *)udp, (int)payload, end, 0, 0, 0, 0, 0, 0, 0);
+	taskSpawn("tMotor", 100, 0, 4096, (FUNCPTR) motorTask, &target_steps, &end_tasks, 0, 0, 0, 0, 0, 0, 0, 0);
+	
+	char c;
+	int i = 0;
+	while(1) {
+		i = (i + 1) % 5;
+		target_steps = (i - 2)*512;
+		c =	 (char)getchar();
+		
+		
+		if (c == 'q' || c == 'Q') break;
+	}
+	
+	end_tasks = 1;
+		
 }
