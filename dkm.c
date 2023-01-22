260cddfcdd27fef7b61deeb015615002d935f527
Karanfil, Mustafa Suphi Deniz <karanmus@fel.cvut.cz>
2023-01-22 13:38:19 +0100

    udp communication

--- dkm.c
+++ dkm.c
@@ -1,8 +1,20 @@
 /* includes */
 
 #include "vxWorks.h"
+#include "udp.h"
+#include <taskLib.h>
+/* I think we are only sending one int at each time:*/
+#define payload 4
+static int end;
+static UDP *udp;
 
+void start(char *ip_address, int port) {
+	
+	udp = initudp(ip_address, port);
 
-void start(void) {
-
+	/* MASTER */
+	  sprintf(taskName, "tControlMotor");
+	  TASK_ID tControlMotor = taskSpawn(
+	  taskName, 110, 0, 4096, (FUNCPTR)getDesiredPos,
+	         (UDP *)udp, (int)payload, end, 0, 0, 0, 0, 0, 0, 0);
 }
